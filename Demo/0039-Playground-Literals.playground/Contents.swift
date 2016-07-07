d73bb441a0813fd4b6968579773b1b17b0b1b76a
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2016-07-07 02:40:16 -0400

    Added 42

--- Demo/0039-Playground-Literals.playground/Contents.swift
+++ Demo/0039-Playground-Literals.playground/Contents.swift
@@ -7,3 +7,14 @@ import UIKit
  */
 
 //let color = 
+
+
+struct Type {
+    var x: Int
+    func instanceMethod(y y: Int) -> Int {
+        return x + y
+    }
+}
+
+let f = Type.instanceMethod // f : (Type) -> (y: Int) -> Int
+f(Type(x: 1))(y: 2)         // ==> 3
